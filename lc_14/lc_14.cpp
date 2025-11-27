703323de5d7a5bca570f5324c4af196f4d1e4887
LogicalTrue <59065051+LogicalTrue@users.noreply.github.com>
2025-11-27 14:44:28 -0300

    push

--- lc_14/lc_14.cpp
+++ lc_14/lc_14.cpp
@@ -7,20 +7,29 @@ using namespace std;
 
 class Solution {
 public:
+    //"cir","car"
+    //"flower","flow","flight"
+    //"dog","racecar","car"
     string longestCommonPrefix(vector<string>& strs) {
         string prefix = strs[0];
-        int c = 0;
-       
             for (int x = 1; x < strs.size(); x++) {
-                c = 0;
-                    for (int j = 0; j < strs[x].size(); j++) {
-                            if (strs[x][j] == prefix[j] && prefix[j] && prefix[j] == strs[x][j] && prefix[j] != NULL) {
-                                prefix.push_back(strs[x][j]);
-                            }
-                            else {
-                                prefix.pop_back();
-                                break;
-                            }
+                for (int j = 0; j < prefix.size(); j++) {
+                    if (strs[x][0] != prefix[0]){
+                        prefix = "";
+                        break;
+                    }
+                
+                    char a = strs[x][j];
+                    char b = prefix[j];
+
+                        if(a != b) {
+                            //no esta mal, pero habria que hacer una revision de cuantos caracteres coinciden y de ahi hacer una regresion
+                            //
+                            int res = std::abs((int)strs[x].size() - (int)prefix.size());
+                                for (int r = 0; r < res; r++)
+                                    prefix.pop_back();  
+                                    break;
+                        }
                 }
             }
             return prefix;
@@ -29,7 +38,7 @@ public:
 
 int main()
 {
-    vector<string> strs = { "flower", "flow", "flight" };
+    vector<string> strs = { "flower","flow","flight" };
 
     Solution Solution;
 
